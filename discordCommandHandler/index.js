3e124313530ee33658a1d77aad406487753ee094
Charles Merrick Moss <merrickm@live.com>
2021-10-19 11:18:16 -0400

    Build of handleCommand function

--- discordCommandHandler/index.js
+++ discordCommandHandler/index.js
@@ -1,4 +1,5 @@
 const { verifyKey } = require("discord-interactions");
+const axios = require('axios');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -15,18 +16,48 @@ module.exports = async function (context, req) {
     if (!verifiedRequest) {
         context.log("Invalid request signature");
         return context.res = { 
-            status: 401
+            status: 401,
         };
     } else if (req.body.type == 1) {
         context.log("Message type 1, sending ACK type 1");
         return context.res = { 
-            "type": 1 
+            body: {"type": 1 }
         };
     } else {
-        context.log("Valid request, not type 1 req, sending res 200, type 1");
+        messageBack = await handleCommand(req.body.data.name);
+        context.log("messageBack: " + messageBack);
         return context.res = {
             status: 200, 
-            "type": 1
+            body: {
+                "type": 4,
+                "data": {
+                    "tts": False,
+                    "content": `${messageBack}`,
+                    "embeds": [],
+                    "allow_mentions": {"parse": []}
+                }
+            }
         };
+    }
+}
+
+async function handleCommand(commandName) {
+    switch(commandName) {
+        case 'gameplan':
+            context.log(`Valid request, type: ${req.body.type}`);
+            const playHost = req.body.data.options[0].value;
+            context.log("Have playhost: " + playHost);
+            const game = req.body.data.options[1].value;
+            context.log("Have game: " + game);
+            axios.put('https://www.xboxplaydates.us/ambassadorschedule/discordShowUpdate', 
+                {hostName: `${playHost}`, gameName: `${game}`})
+                .then(res => {
+                    if (res.status(200)) {
+                        context.log("Status 200");
+                        context.log(res.body);
+                        return `${res.body.title} on ${res.body.date} has been updated to ${res.body.game}`;
+                    }
+            });
+            break;
     }
 }
