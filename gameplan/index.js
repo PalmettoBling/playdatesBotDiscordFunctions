1f23edd95c1cd2f793207b017096651023e2581e
Merrick <palmettobling@gmail.com>
2022-05-12 20:16:21 -0400

    Update display

--- gameplan/index.js
+++ gameplan/index.js
@@ -32,6 +32,19 @@ module.exports = async function (context, req) {
                 context.log.error("ERROR", err);
                 throw err;
             }
+        } else if (apiResponse.data.info === 'Show not updated.  Please check spelling or be more specific...') {
+            const responseMessage = apiResponse.data.info;
+            try {
+                await axios.patch(`https://discord.com/api/webhooks/${applicationId}/${interactionToken}/messages/@original`, {
+                    "content": responseMessage
+                },
+                { 
+                    "Content-Type": "application/json"
+                });
+            } catch (err) {
+                context.log.error("ERROR", err);
+                throw err;
+            }
         } else {
             const responseMessage = `The show ${apiResponse.data.title} on ${apiResponse.data.date} has been updated to ${apiResponse.data.game}`;        
             try {
