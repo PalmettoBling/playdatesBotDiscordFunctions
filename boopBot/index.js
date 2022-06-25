f99474c32757219c487aa3b517a00fb5558ecff3
Merrick <palmettobling@gmail.com>
2022-06-25 01:03:44 -0400

    Updated wording on response message

--- boopBot/index.js
+++ boopBot/index.js
@@ -11,31 +11,16 @@ module.exports = async function (context, req) {
 
     const azureResponse = await axios.post(`https://prod-38.eastus.logic.azure.com:443/workflows/e49780c06aaa4377ac3cbdd7000969c2/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=TqKx6pM8fHEPp_VcS_7vyD5Ei3R1zoB1-to0ZixEVV4`);
 
-    if (azureResponse == "200") {
-        const responseMessage = `The bot has been restarted.`;
-        try {
-            await axios.patch(`https://discord.com/api/webhooks/${applicationId}/${interactionToken}/messages/@original`, {
-                "content": responseMessage
-            },
-            { 
-                "Content-Type": "application/json"
-            });
-        } catch (err) {
-            context.log.error("ERROR", err);
-            throw err;
-        }
-    } else {
-        const responseMessage = `${azureResponse.info}`;
-        try {
-            await axios.patch(`https://discord.com/api/webhooks/${applicationId}/${interactionToken}/messages/@original`, {
-                "content": responseMessage
-            },
-            { 
-                "Content-Type": "application/json"
-            });
-        } catch (err) {
-            context.log.error("ERROR", err);
-            throw err;
-        }
+    const responseMessage = `An attempt to restart the show has been made.  Please allow a few minutes while the bot restarts.`;
+    try {
+        await axios.patch(`https://discord.com/api/webhooks/${applicationId}/${interactionToken}/messages/@original`, {
+            "content": responseMessage
+        },
+        { 
+            "Content-Type": "application/json"
+        });
+    } catch (err) {
+        context.log.error("ERROR", err);
+        throw err;
     }
 }
